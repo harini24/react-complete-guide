55b5008e4e9c4fb6aef89a52d8783c401d14793d
harini <harinisiva1997@gmail.com>
2021-05-12 23:39:16 +0530

    writing more complex JSX

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,11 @@
 function ExpenseItem(){
-    return <h2>Expense Item</h2>
+    return (
+        <div>
+            <div>5/12/2021</div>
+            <h2>Car Insurance</h2>
+            <div>250 $</div>
+        </div>
+    )
 }
 
 export default ExpenseItem

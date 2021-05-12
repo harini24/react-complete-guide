ae25b556f0a986b835627e731aae96de2df6e74f
harini <harinisiva1997@gmail.com>
2021-05-12 23:34:19 +0530

    building first custom component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
+import ExpenseItem from'./components/ExpenseItem'
 function App() {
   return (
     <div>
       <h1>My first learning</h1>
+      <ExpenseItem/>
     </div>
   );
 }
